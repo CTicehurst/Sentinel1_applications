d72787bb6dcd620c8bc9a87b7f7535f0931825af
CTicehurst <39070987+CTicehurst@users.noreply.github.com>
2019-09-03 13:13:40 +1000

    Update S1_GRD_ReadFiles_plusSARAid_onHPC.py

--- SAR_ARD_processing/S1_GRD_ReadFiles_plusSARAid_onHPC.py
+++ SAR_ARD_processing/S1_GRD_ReadFiles_plusSARAid_onHPC.py
@@ -11,21 +11,10 @@ def quicklook_to_filepath(qlurl):
 
 # SARA URL details providing area of interest, dates, and S1 product. Input the longitude / latitude corner points of area of interest within 'POLYGON(())'
 
-#TestURL = "https://copernicus.nci.org.au/sara.server/1.0/api/collections/S1/search.json?_pretty=1" \
-#          "&startDate=2018-09-01&completionDate=2018-10-01&productType=GRD&sensorMode=IW" \
-#          "&geometry=POLYGON((150.4 -26.2, 151.5 -26.2, 151.5 -27.0, 150.4 -27.0, 150.4 -26.2))"
-		  
-#TestURL = "https://copernicus.nci.org.au/sara.server/1.0/api/collections/S1/search.json?_pretty=1" \
-#          "&startDate=2016-07-01&completionDate=2016-07-31&productType=GRD&sensorMode=IW" \
-#          "&geometry=POLYGON((123.5 -17.5, 124.5 -17.5, 124.5 -18.5, 123.5 -18.5, 123.5 -17.5))"
-
-# McArthur River Mine
+# Location details
 TestURL = "https://copernicus.nci.org.au/sara.server/1.0/api/collections/S1/search.json?_pretty=1" \
           "&startDate=2019-06-20&completionDate=2019-08-20&productType=GRD&sensorMode=IW" \
           "&geometry=POLYGON((136.02 -16.36, 136.15 -16.36, 136.15 -16.49, 136.02 -16.49, 136.02 -16.36))"
-#TestURL = "https://copernicus.nci.org.au/sara.server/1.0/api/collections/S1/search.json?_pretty=1" \
-#          "&startDate=2019-07-01&completionDate=2019-08-20&productType=SLC&sensorMode=IW" \
-#          "&geometry=POLYGON((136.02 -16.36, 136.15 -16.36, 136.15 -16.49, 136.02 -16.49, 136.02 -16.36))"
 
 print("TestURL=",TestURL)
 
@@ -63,12 +52,7 @@ while nresult>0:
 print("filepaths =", filepaths[0])
 
 outfile = 'Z:/MRM/S1_filenames_GRD_IW_MRM_wSARAid_lateJunTo20Aug2019.txt'
-#outfile = 'Z:/MRM/S1_filenames_SLC_IW_MRM_wSARAid_JulTo20Aug2019.txt'
-#outfile = 'Z:/work/cjt/SAR/SAR_Cube/S1_filenames_GRD_IW_Fitzroy_wSARAid.txt'
-#outfile = 'Z:/SAR/SAR_Cube/S1_filenames_GRD_IW_Surat_wSARAid.txt'
-#outfile = 'C:/Users/tic013/S1_processing/S1_filenames_GRD_IW_Surat_wSARAid.txt'
+
 SARA_id += [file_url[i].split('/')[len(file_url[i].split('/'))-2] for i in range(len(file_url))]
 with open(outfile,'w') as op:
-    #op.writelines(map(lambda x: x + '\n', filter(None,filepaths)))
     op.writelines(map(lambda x, y: x + ', ' + y + '\n', filter(None,filepaths), filter(None,SARA_id)))
-
