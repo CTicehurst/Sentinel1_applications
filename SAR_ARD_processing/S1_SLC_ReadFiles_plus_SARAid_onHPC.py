64dee88c0ab0a9a56efd5d4c0b87d611794dd542
CTicehurst <39070987+CTicehurst@users.noreply.github.com>
2019-09-03 13:18:30 +1000

    Update S1_SLC_ReadFiles_plus_SARAid_onHPC.py

--- SAR_ARD_processing/S1_SLC_ReadFiles_plus_SARAid_onHPC.py
+++ SAR_ARD_processing/S1_SLC_ReadFiles_plus_SARAid_onHPC.py
@@ -15,7 +15,7 @@ def quicklook_to_filepath(qlurl):
 # Location details
 TestURL = "https://copernicus.nci.org.au/sara.server/1.0/api/collections/S1/search.json?_pretty=1" \
           "&startDate=2019-06-20&completionDate=2019-08-20&productType=SLC&sensorMode=IW" \
-          "&geometry=POLYGON((136.02 -16.36, 136.15 -16.36, 136.15 -16.49, 136.02 -16.49, 136.02 -16.36))"
+          "&geometry=POLYGON((131.02 -16.36, 132.15 -16.36, 132.15 -16.49, 131.02 -16.49, 131.02 -16.36))"
 
 print("TestURL=",TestURL)
 
@@ -52,7 +52,7 @@ while nresult>0:
 # information is used to create the full filepath for accessing the files on the NCI thredds server ****
 print("filepaths =", filepaths[0])
 
-outfile = 'Z:/MRM/S1_filenames_SLC_IW_MRM_wSARAid_lateJunTo20Aug2019.txt'
+outfile = 'Z:/SAR/S1_filenames_SLC_IW_wSARAid_lateJunTo20Aug2019.txt'
 
 SARA_id += [file_url[i].split('/')[len(file_url[i].split('/'))-2] for i in range(len(file_url))]
 with open(outfile,'w') as op:
